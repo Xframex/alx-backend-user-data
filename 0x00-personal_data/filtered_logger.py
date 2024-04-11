8c80fe91453e2c1135c4760ca630908db08b2dea
Xframex <loukili.issmael@.com>
2024-04-11 15:27:12 +0000

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,5 +2,7 @@
 
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
-    return re.sub(r'(?<!\S)(' + '|'.join(fields) + r')=[^;]*', lambda x: x.group(1) + '=' + redaction, message)
+    return re.sub(r'(?<!\S)(' + '|'.join(fields) + r')=[^;]*',
+                  lambda x: x.group(1) + '=' + redaction, message)
