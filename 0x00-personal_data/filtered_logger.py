7d5d156512b3b928264a5ecc857ada364dcd3969
Xframex <loukili.issmael@.com>
2024-04-11 15:34:36 +0000

    task 1 fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    return re.sub(r'(?<!\S)(' + '|'.join(fields) + r')=[^;]*',
-                  lambda x: x.group(1) + '=' + redaction, message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Replace sensitive information from message with redaction
+    and use regex pattern to match the fields.
+    """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
