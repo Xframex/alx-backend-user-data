c4538c29e442879b1ee0c7756b3d9e5023f6ca99
Xframex <loukili.issmael@.com>
2024-04-11 15:54:34 +0000

    task 3 fix pyco

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python3
 
+import logging
 import re
 from typing import List
 
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the record with redaction.
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
-    Replace sensitive information from message with redaction
-    and use regex pattern to match the fields.
+    Replace sensitive information from message with redaction and
+    use regex pattern to match the fields.
     """
     for f in fields:
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
-    return message
+    return message.strip()
