d79a2c4b1cd4c6f6f9ee8b0017cadc23b4859159
Xframex <loukili.issmael@.com>
2024-04-16 14:43:09 +0000

    fix pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,11 +6,10 @@ from typing import List, TypeVar
 
 
 class Auth:
-    """Auth class
-    """
+    """Auth class"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require authentication
-        """
+        """Require authentication"""
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
         if path[-1] != '/':
@@ -18,15 +17,13 @@ class Auth:
         if path in excluded_paths:
             return False
         return True
-    
+
     def authorization_header(self, request=None) -> str:
-        """Authorization header
-        """
+        """Authorization header"""
         if request is None or 'Authorization' not in request.headers:
             return None
         return request.headers.get('Authorization', None)
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
-        """Current user
-        """
+        """Current user"""
         return None
