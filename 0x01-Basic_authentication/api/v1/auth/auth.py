740038ebd0d21e98534cae90629e0965209dbfee
ismail <125493050+Xframex@users.noreply.github.com>
2024-04-16 14:30:16 +0100

    Add stats endpoint to index.py

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Auth module for the API"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require authentication
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+    
+    def authorization_header(self, request=None) -> str:
+        """Authorization header
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization', None)
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user
+        """
+        return None
