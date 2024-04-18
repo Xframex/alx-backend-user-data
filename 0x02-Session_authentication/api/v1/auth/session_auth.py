0bdf93037e07dd368841727a49fd2af97acb36a2
Xframex <loukili.issmael@.com>
2024-04-18 19:58:57 +0000

    add session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,21 @@
 """Session authentication module for the API.
 """
 from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """Session authentication class.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id.
+        """
+        if user_id is None or type(user_id) != str:
+            return None
+        user_id = str(uuid.uuid4())
+        self.user_id_by_session_id[user_id] = user_id
+        return user_id
