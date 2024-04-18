c02c448ed5aab2cc49a6939d2e969f09d22a643b
Xframex <loukili.issmael@.com>
2024-04-18 20:26:38 +0000

    create user id for session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,16 @@ class SessionAuth(Auth):
         user_id = str(uuid.uuid4())
         self.user_id_by_session_id[user_id] = user_id
         return user_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the user id of the user associated with
+        a given session id.
+        """
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
+    
+    def current_user(self, request=None) -> User:
+        """Retrieves the user associated with the request.
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
