d2740a89c7c445ed70bb502a112f974c9d85a0ab
Xframex <loukili.issmael@.com>
2024-04-19 17:36:38 +0000

    session Authentification

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Session authentication module for the API.'''
+
+from flask import abort, request, jsonify
+from api.v1.views import app_views
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    '''auth login routr'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].id)
+    SESSION_NAME = 'session_id'
+    response = jsonify(user[0].to_json())
+    cookie = os.getenv('SESSION_NAME') 
+    response.set_cookie(SESSION_NAME, session_id)
+    return response
