ec72338a158ddc8d8deb2aff6c7c41e7af228fd2
Xframex <loukili.issmael@.com>
2024-04-25 19:59:12 +0000

    secret password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,6 @@
 from base64 import b64encode
 
 
-def _hash_password(password: str) -> str:
+def _hash_password(password: str) -> bytes:
     '''hashes a password'''
     return b64encode(password.encode()).decode()
