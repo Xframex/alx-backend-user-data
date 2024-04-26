1c2fedc54deaa58fa8e584bed71c5078b6b84a18
Xframex <loukili.issmael@.com>
2024-04-26 01:19:48 +0000

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,6 +18,17 @@ def index() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user() -> str:
+    """POST /users method"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        Auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
